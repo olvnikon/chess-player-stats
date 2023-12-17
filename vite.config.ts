95c4c64903a22d156b543bd50fc0af2a5c910904
olvnikon <olvnikon@gmail.com>
2023-12-17 10:16:27 +0100

    fix: change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://olvnikon.github.io/chess-player-stats/',
+  base: 'https://olvnikon.github.io/chess-player-stats/dist/',
   plugins: [react()],
 });
