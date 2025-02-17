1b25c3432d7e868416b56c4c9449d25dc2782f5b
olvnikon <olvnikon@gmail.com>
2025-02-17 10:29:42 +0100

    chore: testing redirection

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { PlayerStats } from './PlayerStats';
 const usersToTrack = ['DiomaNik', 'DiomaNikTurniej'];
 
 function App() {
+  window.location.href = 'market://details?id=com.ubs.neo';
   return (
     <div
       style={{
